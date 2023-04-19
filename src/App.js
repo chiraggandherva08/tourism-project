520f9e8fa7bb215d14a5ff8184f0a105ac0b52bc
Chirag Gandherva <chiraggandherva08@gmail.com>
2023-04-19 13:32:38 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,17 +6,21 @@ import Translator from './Components/Translator/Translator';
 import SustainableDevelopment from './Components/SustainableDevelopment/SustainableDevelopment';
 import Reviews from './Components/Reviews/Reviews';
 import Weather from './Components/Weather/weather';
+import Preview from './Components/MetroCities/Preview';
+import ViewMetroMaps from './Components/MetroCities/ViewMetroMaps';
 
 function App() {
   return (
     <div className="App">
       <Menu/>
       <Header/>
+      <Preview/>
       <Translator/> 
       <Map/>
       <SustainableDevelopment/>
       <Reviews/>
       <Weather/>
+      <ViewMetroMaps/>
     </div>
   );
 }
